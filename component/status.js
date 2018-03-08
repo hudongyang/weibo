71fb825c562009aa45e37082ba12f1aa726bcecc
hudongyang <whudongyang@163.com>
2018-03-08 11:29:11 +0800

    weibo

--- component/status.js
+++ component/status.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import moment from 'moment'
-import style from './status'
-console.log(style)
+import style from './status.css'
+
 export default class Status extends React.Component {
     render() {
         return (
-            <li>
-                <div className="desc">
+            <li className={style.status_item}>
+                <div className={style.desc}>
                     <img className={style.avatar} src={this.user.profile_image_url} />
-                    <div className="user-info">
+                    <div className={style.user_info}>
                         <strong>{this.user.name}</strong>
-                        <span className="timestamp">{moment(this.props.status.created_at).format('YYYY MM DD hh:mm:ss')}</span>
+                        <span className={style.timestamp}>{moment(this.props.status.created_at).format('YYYY MM DD hh:mm:ss')}</span>
                     </div>
                 </div>
+
+                <div className={style.content}>{this.props.status.text}</div>
             </li>
         )
     }
